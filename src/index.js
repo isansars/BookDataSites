b39179f0fca4f443257a0abeb4367faa569b659a
user name <user email>
2020-12-13 13:16:06 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+        jangan ubah kode di bawah ini ya!
+*/
+
+import "regenerator-runtime/runtime.js";
+import "./styles/style.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
+import main from "./scripts/main";
+
+main();
